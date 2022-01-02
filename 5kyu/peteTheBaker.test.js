32f3ca22a75a4cee4c605af0e60bee9414879a09
Rick Hallett <rickhallett@live.co.uk>
2022-01-02 10:55:13 +0000

    add chai tests

--- 5kyu/peteTheBaker.test.js
+++ 5kyu/peteTheBaker.test.js
@@ -14,25 +14,21 @@ cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flo
 
  */
 
+const chai = require('chai')
+const assert = chai.assert
+chai.config.truncateThreshold = 0
+
 describe('description example', function () {
     var recipe, available
 
     it('pass example tests', function () {
         recipe = { flour: 500, sugar: 200, eggs: 1 }
         available = { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
-        Test.assertEquals(
-            cakes(recipe, available),
-            2,
-            'Wrong result for example #1'
-        )
+        assert.equal(cakes(recipe, available), 2, 'Wrong result for example #1')
 
         recipe = { apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 }
         available = { sugar: 500, flour: 2000, milk: 2000 }
-        Test.assertEquals(
-            cakes(recipe, available),
-            0,
-            'Wrong result for example #2'
-        )
+        assert.equal(cakes(recipe, available), 0, 'Wrong result for example #2')
     })
 })
 
