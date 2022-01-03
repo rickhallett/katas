ad34fb119642f07bbe154cde0a077fdc18448c4d
Rick Hallett <rickhallett@live.co.uk>
2022-01-03 09:31:24 +0000

    add 4kyu: sudoku validator

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  trailingComma: "es5",
-  tabWidth: 4,
-  semi: false,
-  singleQuote: true,
-};
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    printWidth: 80,
+}
